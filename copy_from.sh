a3987d78c868b3233314d66343abf73aafb27563
Psyhackological <konradkon@duck.com>
2022-05-13 18:28:53 +0200

    Added comments to make it more readable

--- copy_from.sh
+++ copy_from.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
+# ZSH
 mkdir -p zsh
 cp ~/.zshrc zsh
+
+# BASH
 mkdir -p bash
 cp ~/.bashrc bash
+
+# MPV
 mkdir -p mpv
 cp ~/.config/mpv/mpv.conf mpv
 cp ~/.config/mpv/input.conf mpv
+
+# ALACRITTY
 mkdir -p dotfiles/alacritty
 cp ~/.config/alacritty/alacritty.yml alacritty
+
+# VIM
 mkdir -p vim
 cp ~/.vimrc vim
+
+# ZATHURA
 mkdir -p zathura
 cp ~/.config/zathura/zathurarc zathura
+
+# TASKWARRIOR
 mkdir -p taskwarrior
 cp ~/.taskrc taskwarrior
 cp ~/.task/nord.theme taskwarrior
