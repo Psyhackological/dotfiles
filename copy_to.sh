455fc172b6eb917a455bb6185f779b3704c06764
Psyhackological <konradkon@duck.com>
2022-05-13 18:19:05 +0200

    Added 2 bash scripts to help me manage dotfiles

---
+++ copy_to.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ZSH
+cp zsh/.zshrc ~
+
+# BASH
+cp bash/.bashrc  ~
+
+# MPV
+mkdir -p ~/.config/mpv
+cp mpv/mpv.conf ~/.config/mpv
+cp mpv/input.conf ~/.config/mpv
+
+# ALACRITTY
+mkdir -p ~/.config/alacritty 
+cp alacritty/alacritty.yml ~/.config/alacritty 
+
+# VIM
+cp vim/.vimrc ~
+
+# ZATHURA
+mkdir -p ~/.config/zathura
+cp zathura/zathurarc ~/.config/zathura/zathurarc
+
+# TASKWARRIOR
+mkdir -p ~/.task
+cp taskwarrior/.taskrc ~/.taskrc
+cp taskwarrior/nord.theme ~/.task
+cp taskwarrior/on-modify.timewarrior ~/.task/hooks
+chmod u+x ~/.task/hooks/on-modify.timewarrior
